bf15ff6db0f00a836c6cf8f00ad5963e2c8c53b9
MuhammedTahaBezci <bezcitaha@gmail.com>
2024-07-16 15:39:56 +0300

    ilk commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import CourseInput from './components/CourseInput';
+
+export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  
+  const startModal = () =>{
+    setModalIsVisible(true);
+  }
+
+  return (
+    <>
+    <StatusBar style="light" />
+    <View style={styles.container}>
+        <Button title = 'Kurs Ekle' color="darkcyan" onPress={startModal}/>
+        <CourseInput 
+        visible = {modalIsVisible}/>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 25
+  },
+});
