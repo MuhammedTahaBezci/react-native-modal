78a2c78312190bfa249e8fc8a8aac1b0a7e5354d
MuhammedTahaBezci <bezcitaha@gmail.com>
2024-07-17 19:23:23 +0300

    modal kodlandı

--- components/CourseInput.js
+++ components/CourseInput.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Modal, Image, TextInput, Button } from "react-native";
 import React from "react";
 
 export default function CourseInput({visible}) {
   return (
-    <View>
-      <Text>CourseInput</Text>
-    </View>
+    <Modal
+        animationType="slide"
+        visible={visible}
+        >
+        <View style={styles.inputContainer}>
+          <Image style={styles.image} source = {require(
+            '../assets/images/logo.png'
+          )}/>
+          <TextInput style={styles.textInput} placeholder="Yeni Kurs Ekle"/>
+          <View style={styles.modalView}>
+            <View style={styles.Buton}>
+              <Button title="İptal Et" color="darkred"/>
+            </View>
+            <View style={styles.Buton}>
+              <Button title="İptal Et" color="darkcyan"/>
+            </View>          
+          </View>
+        </View>
+      </Modal>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer:{
+
+  },
+  image:{
+
+  },
+  textInput:{
+
+  },
+  buton:{
+
+  },
+});
